9d5aaa7f456f8ce7ff78a8e6f9d4c4b22f3f0088
Lee Wenqian <zzw890827@gmail.com>
2018-08-08 11:38:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar/>
+      <div className="content">
+        <NameWithHandle/>
+        <Message/>
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img src="https://www.gravatar.com/avatar/nothing"
+         className="avatar"
+         alt="avatar"/>
+  );
+}
+
+function Message() {
+  return (
+    <div className="message">
+      This is less than 140 characters.
+    </div>
+  );
+}
+
+function NameWithHandle() {
+  return (
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+
+ReactDOM.render(
+  <Tweet/>,
+  document.getElementById('root')
+);
